a7da6bdc7f0930f30f143bf152b982202efc7bd4
Steven <s.spencerletts@gmail.com>
2023-03-06 10:03:15 +0000

    submit form added

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ import "./style.css";
 
 let myLibrary = [];
 
+const submit = document.getElementById("submit-book");
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+  let form = document.getElementById("book-form");
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
+  const book = new Book(title, author, pages, read);
+  addBookToLibrary(book);
+  console.log(myLibrary);
+  displayBooks();
+});
+
 function Book(title, author, pages, read = false) {
   this.title = title;
   this.author = author;
@@ -20,13 +34,16 @@ function displayBooks() {
   let tableBody = document.getElementById("books");
   for (let i = 0; i < myLibrary.length; i++) {
     // create row
+    if (tableBody.rows.length > i + 1) {
+      continue;
+    }
     let row = tableBody.insertRow(-1);
     // create cells
     let c1 = row.insertCell(0);
     let c2 = row.insertCell(1);
     let c3 = row.insertCell(2);
     let c4 = row.insertCell(3);
-    // delete btn cell
+    // delete btn cell and delete btn logic
     let c5 = row.insertCell(4);
 
     let delBtn = document.createElement("button");
@@ -51,14 +68,9 @@ function deleteBook(bookTitle) {
   for (let i = 0; i < myLibrary.length; i++) {
     if (myLibrary[i].title == bookTitle) {
       myLibrary.splice(i, 1);
+      break;
     }
   }
 }
-
-const book1 = new Book("a", "b", 200, "yes");
-const book2 = new Book("c", "d", 200, "no");
-
-addBookToLibrary(book1);
-addBookToLibrary(book2);
 
 displayBooks();
